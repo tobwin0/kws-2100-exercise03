21666a7a795eb04338bcd83f53aa27c6af0ce2ea
tobwin0 <184448769+tobwin0@users.noreply.github.com>
2026-01-20 12:29:07 +0100

    inital react project

---
+++ src/app/application.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Application() {
+  return (
+    <>
+      <div>Hei på deg din luring</div>
+    </>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./app/application.js";
+
+createRoot(document.getElementById("root")!).render(<Application />);
