a8444a593ebb25cfbcc96ca49600c5e75d12fe20
tobwin0 <184448769+tobwin0@users.noreply.github.com>
2026-01-20 13:36:34 +0100

    Added simple OSM map

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { OSM } from "ol/source.js";
+import TileLayer from "ol/layer/Tile.js";
+import { View, Map, Feature, MapBrowserEvent } from "ol";
+import { useGeographic } from "ol/proj.js";
+
+// CSS
+import "ol/ol.css";
+import "./application.css";
+
+useGeographic();
+
+const map = new Map({
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [11, 59], zoom: 8 }),
+});
 
 export function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+
   return (
     <>
-      <div>Hei på deg din luring</div>
+      <h1>Regioner i Norge</h1>
+      <main>
+        {/* OSM Map (tile map / puslespill kart) */}
+        <div ref={mapRef}></div>
+      </main>
     </>
   );
 }
