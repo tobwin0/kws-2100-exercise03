f78709a8a5ecd06c5dc4f2bd61a72623c459ba63
tobwin0 <184448769+tobwin0@users.noreply.github.com>
2026-01-28 16:52:33 +0100

    Added aerial photos and kartverkets map

--- src/layers/backgroundLayerSelect.tsx
+++ src/layers/backgroundLayerSelect.tsx
@@ -1,11 +1,53 @@
 import { Layer } from "ol/layer.js";
-import { OSM, StadiaMaps } from "ol/source.js";
+import { OSM, StadiaMaps, WMTS } from "ol/source.js";
 import { useEffect, useState } from "react";
 import TileLayer from "ol/layer/Tile.js";
+import { WMTSCapabilities } from "ol/format.js";
+import { optionsFromCapabilities } from "ol/source/WMTS.js";
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
+
+proj4.defs(
+  "EPSG:25833",
+  "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+);
+register(proj4);
 
 const OSMlayer = new TileLayer({ source: new OSM() });
+
 const stadiaMapsLayer = new TileLayer({
   source: new StadiaMaps({ layer: "alidade_smooth_dark" }),
+});
+
+const kartverket = new TileLayer({});
+const kartverketUrl =
+  "https://cache.kartverket.no/v1/wmts/1.0.0/WMTSCapabilities.xml";
+fetch(kartverketUrl).then(async (response) => {
+  const parser = new WMTSCapabilities();
+  kartverket.setSource(
+    new WMTS(
+      optionsFromCapabilities(parser.read(await response.text()), {
+        layer: "toporaster",
+        matrixSet: "webmercator",
+      })!,
+    ),
+  );
+});
+
+const flyfoto = new TileLayer({});
+const flyfotoUrl =
+  "http://opencache.statkart.no/gatekeeper/gk/gk.open_nib_utm33_wmts_v2?SERVICE=WMTS&REQUEST=GetCapabilities";
+fetch(flyfotoUrl).then(async (response) => {
+  const parser = new WMTSCapabilities();
+  const capabilities = parser.read(await response.text());
+  flyfoto.setSource(
+    new WMTS(
+      optionsFromCapabilities(capabilities, {
+        layer: "Nibcache_UTM33_EUREF89_v2",
+        matrixSet: "default028mm",
+      })!,
+    ),
+  );
 });
 
 export function BackgroundLayerSelect({
@@ -27,6 +69,12 @@ export function BackgroundLayerSelect({
     if (backgroundLayerValue === "stadia") {
       setBackgroundLayer(stadiaMapsLayer);
     }
+    if (backgroundLayerValue === "kartverket") {
+      setBackgroundLayer(kartverket);
+    }
+    if (backgroundLayerValue === "aerial") {
+      setBackgroundLayer(flyfoto);
+    }
   }, [backgroundLayerValue]);
   return (
     <select
@@ -35,6 +83,8 @@ export function BackgroundLayerSelect({
     >
       <option value={"osm"}>OpenStreetMap bakgrunn</option>
       <option value={"stadia"}>Stadia bakgrunnskart</option>
+      <option value={"kartverket"}>Kartverkets bakgrunnskart</option>
+      <option value={"aerial"}>Kartverkets flyfoto bakgrunnskart</option>
     </select>
   );
 }
